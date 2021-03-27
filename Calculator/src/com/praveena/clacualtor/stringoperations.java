805cec418557af2cf614a49b0906646bd881f03a
prave <prave@LAPTOP-SH4KBDL9>
2021-03-27 13:53:17 -0500

    Added String operations file-second commit

---
+++ Calculator/src/com/praveena/clacualtor/stringoperations.java
@@ -0,0 +1,12 @@
+package com.praveena.clacualtor;
+
+public class stringoperations {
+	
+	public void stringToLower(String s) {
+	
+	String s1 = s.toLowerCase();
+	System.out.println("String in Lower Case="+s1);
+	
+	}
+
+}
